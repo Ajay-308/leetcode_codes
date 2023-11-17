4f85ff55304a4a93b228d0c6694f50ce4eed7ef1
Ajay <112794893+Ajay-308@users.noreply.github.com>
2023-11-17 20:30:14 +0530

    Update valid soduku.cpp

--- Backtracking/valid soduku.cpp
+++ Backtracking/valid soduku.cpp
@@ -12,6 +12,9 @@ public:
         }
         return true;
     }
+/*For non-empty cells (not containing '.'), it calls the isValid function to check the validity of that specific cell.
+If any cell violates Sudoku rules, it immediately returns false.
+If all cells pass the validity check, it returns true.*/
 
     bool isValid(vector<vector<char>>& board, int row, int col, char c) {
         for (int i = 0; i < 9; i++) {
@@ -30,3 +33,9 @@ public:
         return true;
     }
 };
+
+/*It checks three conditions to ensure that placing the number c in the given cell (row, col) is valid.
+The first two if statements verify if c exists in the same row or column but in different cells. If it does, the placement is invalid.
+The third if block checks the 3x3 subgrid containing the current cell. It calculates the subgrid's top-left cell and then checks if c already exists within that subgrid.
+If any of these conditions fail for the given c at position (row, col), the function returns false. Otherwise, it returns true.
+This combination of isValidSudoku and isValid functions allows for the comprehensive checking of each cell against the Sudoku rules, ensuring that the entire board is valid.*/
