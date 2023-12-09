8f436ac021b450d5a295f0f6af0b2c06ec5a0b78
Ajay <112794893+Ajay-308@users.noreply.github.com>
2023-12-09 23:54:53 +0530

    Update reverseString.py

--- String/reverseString.py
+++ String/reverseString.py
@@ -12,3 +12,21 @@ def reversebyStack(str):
     for i in range(len(str)):
        # Pop the charcters of stack into the original string.
         str[i] = stack.pop()
+
+
+#using 2 pointers 
+
+def reverseStr(str):
+    n = len(str)
+ 
+    i, j = 0, n-1
+ 
+    # Swap character starting from
+    # two corners
+    # i is the left pointer and j is the right pointer
+    while i < j:
+        str[i], str[j] = str[j], str[i]
+ 
+        i += 1
+        j -= 1
+ 
